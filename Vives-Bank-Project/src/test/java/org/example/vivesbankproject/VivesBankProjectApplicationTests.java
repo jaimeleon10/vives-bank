307a300b2f81b95c36a190230dad059d7b83fe35
Jaimelen10 <jleonmulero@gmail.com>
2024-11-14 17:04:54 +0100

    Creacion del proyecto

---
+++ Vives-Bank-Project/src/test/java/org/example/vivesbankproject/VivesBankProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.vivesbankproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VivesBankProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
