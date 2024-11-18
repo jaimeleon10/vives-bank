0ea29ee96f92e7ab1dd2c7f40568d9731379de7a
Mario de Domingo Alvarez <wolverine.mda.307@gmail.com>
2024-11-18 22:25:53 +0100

    added user model

---
+++ Vives-Bank-Project/src/main/java/org/example/vivesbankproject/cuenta/models/TipoCuenta.java
@@ -0,0 +1,40 @@
+package org.example.vivesbankproject.cuenta.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Builder
+@Entity
+@Table(name = "TIPOS_CUENTA")
+@NoArgsConstructor
+@AllArgsConstructor
+public class TipoCuenta {
+    @Id
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    @NotBlank(message = "El nombre del tipo de cuenta no puede estar vacío")
+    private String nombre;
+
+    @Column(nullable = false)
+    @Digits(integer = 3, fraction = 2, message = "El interés debe ser un número válido")
+    @PositiveOrZero(message = "El interés no puede ser negativo")
+    private double interes;
+
+    @CreationTimestamp
+    @Column(updatable = false, nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    @Builder.Default
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @UpdateTimestamp
+    @Column(updatable = true, nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    @Builder.Default
+    private LocalDateTime updatedAt = LocalDateTime.now();
+}
