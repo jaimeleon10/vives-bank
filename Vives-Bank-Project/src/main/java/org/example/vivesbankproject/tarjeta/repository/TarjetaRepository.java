51da1879124e59b9a18e23ab9e79197d03f498cc
Mario de Domingo Alvarez <wolverine.mda.307@gmail.com>
2024-11-19 20:45:46 +0100

    create TarjetaRepository

---
+++ Vives-Bank-Project/src/main/java/org/example/vivesbankproject/tarjeta/repository/TarjetaRepository.java
@@ -0,0 +1,11 @@
+package org.example.vivesbankproject.tarjeta.repository;
+
+import org.example.vivesbankproject.tarjeta.models.Tarjeta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TarjetaRepository extends JpaRepository<Tarjeta, UUID> {
+}
