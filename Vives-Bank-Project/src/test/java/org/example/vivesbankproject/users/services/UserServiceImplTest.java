627d2954fa27a93084cb02b24af3d17331208bca
Alba García Orduña <albag448@gmail.com>
2024-11-20 20:10:20 +0100

    TestUsuario
    Añadiendo Test de UsuarioMapper

---
+++ Vives-Bank-Project/src/test/java/org/example/vivesbankproject/users/services/UserServiceImplTest.java
@@ -0,0 +1,32 @@
+package org.example.vivesbankproject.users.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceImplTest {
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void getByUsername() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
