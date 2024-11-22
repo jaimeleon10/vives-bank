57956c005acdd09cfb17341fbdf468d7ef542121
Jaimelen10 <jleonmulero@gmail.com>
2024-11-22 01:03:27 +0100

    implemented fixes

--- Vives-Bank-Project/src/main/java/org/example/vivesbankproject/VivesBankProjectApplication.java
+++ Vives-Bank-Project/src/main/java/org/example/vivesbankproject/VivesBankProjectApplication.java
@@ -1,15 +1,20 @@
 package org.example.vivesbankproject;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
-public class VivesBankProjectApplication {
+public class VivesBankProjectApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(VivesBankProjectApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("\n\n🕹️ SERVER IS RUNNING 🕹️\n\n");
+    }
 }
