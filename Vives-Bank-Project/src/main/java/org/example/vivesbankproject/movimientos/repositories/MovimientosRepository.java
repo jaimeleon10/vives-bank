bb7cace0b9ccac41b94cc8730cf9db47b9a7cc20
Natalia González Álvarez <nagonal2004@gmail.com>
2024-11-22 17:02:37 +0100

    model y repository de movimiento nuevo

--- Vives-Bank-Project/src/main/java/org/example/vivesbankproject/movimientos/repositories/MovimientosRepository.java
+++ Vives-Bank-Project/src/main/java/org/example/vivesbankproject/movimientos/repositories/MovimientosRepository.java
@@ -2,14 +2,10 @@ package org.example.vivesbankproject.movimientos.repositories;
 
 import org.bson.types.ObjectId;
 import org.example.vivesbankproject.movimientos.models.Movimientos;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-import java.util.UUID;
 
 @Repository
 public interface MovimientosRepository extends MongoRepository<Movimientos, ObjectId> {
