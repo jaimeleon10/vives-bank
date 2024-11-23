8f24b34a08d246290ae053c549d7c97916a84326
Jaimelen10 <jleonmulero@gmail.com>
2024-11-23 17:12:50 +0100

    fixed cuenta mappers and dtos

--- Vives-Bank-Project/src/main/java/org/example/vivesbankproject/cuenta/dto/cuenta/CuentaResponse.java
+++ Vives-Bank-Project/src/main/java/org/example/vivesbankproject/cuenta/dto/cuenta/CuentaResponse.java
@@ -4,7 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.example.vivesbankproject.cuenta.dto.tipoCuenta.TipoCuentaResponse;
 import org.example.vivesbankproject.cuenta.models.TipoCuenta;
+import org.example.vivesbankproject.tarjeta.dto.TarjetaResponse;
 import org.example.vivesbankproject.tarjeta.models.Tarjeta;
 
 import java.math.BigDecimal;
@@ -17,7 +19,7 @@ public class CuentaResponse {
     private String guid;
     private String iban;
     private BigDecimal saldo;
-    private TipoCuenta tipoCuenta;
-    private Tarjeta tarjeta;
+    private TipoCuentaResponse tipoCuenta;
+    private TarjetaResponse tarjeta;
     private Boolean isDeleted;
 }
