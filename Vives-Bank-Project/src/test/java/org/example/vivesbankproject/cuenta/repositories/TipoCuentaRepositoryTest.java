e9dd38913a7ca343e3d72d469da40ea767b88f26
Natalia González Álvarez <nagonal2004@gmail.com>
2024-11-23 17:31:48 +0100

    arreglados test de tcuenta repo

--- Vives-Bank-Project/src/test/java/org/example/vivesbankproject/cuenta/repositories/TipoCuentaRepositoryTest.java
+++ Vives-Bank-Project/src/test/java/org/example/vivesbankproject/cuenta/repositories/TipoCuentaRepositoryTest.java
@@ -3,15 +3,20 @@ package org.example.vivesbankproject.cuenta.repositories;
 import org.example.vivesbankproject.cuenta.models.TipoCuenta;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
+@ExtendWith(SpringExtension.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class TipoCuentaRepositoryTest {
 
     @Autowired
@@ -48,6 +53,6 @@ class TipoCuentaRepositoryTest {
     void findByNombreNotFound() {
         var result = repository.findByNombre("ahorro");
 
-        assertNull(result.orElse(null));
+        assertFalse(result.isPresent());
     }
 }
