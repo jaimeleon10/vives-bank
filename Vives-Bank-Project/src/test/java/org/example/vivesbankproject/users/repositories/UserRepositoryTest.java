f32e35a1279814511920b251b2efface08cea4e2
Natalia González Álvarez <nagonal2004@gmail.com>
2024-11-23 16:09:31 +0100

    arreglado repositorio test de user

--- Vives-Bank-Project/src/test/java/org/example/vivesbankproject/users/repositories/UserRepositoryTest.java
+++ Vives-Bank-Project/src/test/java/org/example/vivesbankproject/users/repositories/UserRepositoryTest.java
@@ -10,13 +10,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @DataJpaTest
 @ExtendWith(SpringExtension.class)
-public class UserRepositoryTest {
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class UserRepositoryTest {
 
     @Autowired
     private UserRepository userRepository;
