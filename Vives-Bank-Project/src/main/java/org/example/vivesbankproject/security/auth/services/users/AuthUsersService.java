ef72679cf806bdae16bd099dd007b862698fe373
Álvaro Herero <alvaro.herrero11x@gmail.com>
2024-11-25 21:30:30 +0100

    refactor
    eliminado para no "duplicar" codigo e implementado en el dominio de user

--- Vives-Bank-Project/src/main/java/org/example/vivesbankproject/security/auth/services/users/AuthUsersService.java
+++
@@ -1,9 +0,0 @@
-package org.example.vivesbankproject.security.auth.services.users;
-
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface AuthUsersService extends UserDetailsService {
-    @Override
-    UserDetails loadUserByUsername(String username);
-}
