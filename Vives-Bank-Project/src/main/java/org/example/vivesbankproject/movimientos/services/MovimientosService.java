c37e53463cea472744bb0f54e9c16a9156977a76
Álvaro Herero <alvaro.herrero11x@gmail.com>
2024-11-26 20:43:22 +0100

    New Movimientos Service Implemented

--- Vives-Bank-Project/src/main/java/org/example/vivesbankproject/movimientos/services/MovimientosService.java
+++ Vives-Bank-Project/src/main/java/org/example/vivesbankproject/movimientos/services/MovimientosService.java
@@ -1,19 +1,21 @@
 package org.example.vivesbankproject.movimientos.services;
 
 import org.bson.types.ObjectId;
+import org.example.vivesbankproject.movimientos.dto.MovimientoRequest;
+import org.example.vivesbankproject.movimientos.dto.MovimientoResponse;
 import org.example.vivesbankproject.movimientos.models.Movimiento;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface MovimientosService {
-    Page<Movimiento> getAll(Pageable pageable);
+    Page<MovimientoResponse> getAll(Pageable pageable);
 
-    Movimiento getById(ObjectId _id);
+    MovimientoResponse getById(ObjectId _id);
 
-    Movimiento getByGuid(String guidMovimiento);
+    MovimientoResponse getByGuid(String guidMovimiento);
 
-    Movimiento getByClienteGuid(String idCliente);
+    MovimientoResponse getByClienteGuid(String idCliente);
 
-    Movimiento save(MovimientoRequest movimientoRequest);
+    MovimientoResponse save(MovimientoRequest movimientoRequest);
 
 }
--- Vives-Bank-Project/src/main/java/org/example/vivesbankproject/movimientos/services/MovimientosServiceImpl.java
+++ Vives-Bank-Project/src/main/java/org/example/vivesbankproject/movimientos/services/MovimientosServiceImpl.java
@@ -2,21 +2,15 @@ package org.example.vivesbankproject.movimientos.services;
 
 import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
-import org.example.vivesbankproject.cliente.dto.ClienteResponse;
-import org.example.vivesbankproject.cliente.exceptions.ClienteNotFound;
-import org.example.vivesbankproject.cliente.mappers.ClienteMapper;
-import org.example.vivesbankproject.cliente.repositories.ClienteRepository;
+
 import org.example.vivesbankproject.cliente.service.ClienteService;
-import org.example.vivesbankproject.cuenta.dto.cuenta.CuentaForClienteResponse;
-import org.example.vivesbankproject.cuenta.mappers.CuentaMapper;
-import org.example.vivesbankproject.cuenta.mappers.TipoCuentaMapper;
+import org.example.vivesbankproject.movimientos.dto.MovimientoRequest;
+import org.example.vivesbankproject.movimientos.dto.MovimientoResponse;
 import org.example.vivesbankproject.movimientos.exceptions.ClienteHasNoMovements;
 import org.example.vivesbankproject.movimientos.exceptions.MovimientoNotFound;
+import org.example.vivesbankproject.movimientos.mappers.MovimientoMapper;
 import org.example.vivesbankproject.movimientos.models.Movimiento;
 import org.example.vivesbankproject.movimientos.repositories.MovimientosRepository;
-import org.example.vivesbankproject.tarjeta.mappers.TarjetaMapper;
-import org.example.vivesbankproject.users.dto.UserResponse;
-import org.example.vivesbankproject.users.mappers.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.CachePut;
@@ -25,85 +19,67 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
-import java.util.*;
-import java.util.stream.Collectors;
-
 @Slf4j
 @Service
 @CacheConfig(cacheNames = {"Movimientos"})
 public class MovimientosServiceImpl implements MovimientosService {
 
     private final ClienteService clienteService;
-    private final ClienteMapper clienteMapper;
     private final MovimientosRepository movimientosRepository;
-    private final UserMapper userMapper;
-    private final CuentaMapper cuentaMapper;
-    private final TipoCuentaMapper tipoCuentaMapper;
-    private final TarjetaMapper tarjetaMapper;
+    private final MovimientoMapper movimientosMapper;
+
+
 
     @Autowired
-    public MovimientosServiceImpl( MovimientosRepository movimientosRepository, ClienteRepository clienteRepository, ClienteMapper clienteMapper, UserMapper userMapper, CuentaMapper cuentaMapper, TipoCuentaMapper tipoCuentaMapper, TarjetaMapper tarjetaMapper, ClienteService clienteService) {
+    public MovimientosServiceImpl( MovimientosRepository movimientosRepository, ClienteService clienteService, MovimientoMapper movimientosMapper) {
         this.clienteService = clienteService;
-        this.tipoCuentaMapper = tipoCuentaMapper;
-        this.tarjetaMapper = tarjetaMapper;
-        this.cuentaMapper = cuentaMapper;
-        this.userMapper = userMapper;
         this.movimientosRepository = movimientosRepository;
-
-        this.clienteMapper = clienteMapper;
-
+        this.movimientosMapper = movimientosMapper;
     }
 
     @Override
-    public Page<Movimiento> getAll(Pageable pageable) {
+    public Page<MovimientoResponse> getAll(Pageable pageable) {
         log.info("Encontrando todos los Movimientos");
-        return movimientosRepository.findAll(pageable);
+        var movimientos = movimientosRepository.findAll(pageable).map(movimientosMapper::toMovimientoResponse);
+        return movimientos;
     }
 
 
     @Override
-    @Cacheable(key = "#guidMovimiento")
-    public Movimiento getById(ObjectId _id) {
+    @Cacheable(key = "#result.guid")
+    public MovimientoResponse getById(ObjectId _id) {
         log.info("Encontrando Movimiento por id: {}", _id);
-        return movimientosRepository.findById(_id).orElseThrow(
-                () -> new MovimientoNotFound(_id)
-        );
+        return movimientosRepository.findById(_id)
+                .map(movimientosMapper::toMovimientoResponse)
+                .orElseThrow(() -> new MovimientoNotFound(_id));
     }
 
     @Override
     @Cacheable(key = "#guidMovimiento")
-    public Movimiento getByGuid(String guidMovimiento) {
+    public MovimientoResponse getByGuid(String guidMovimiento) {
         log.info("Encontrando Movimiento por id: {}", guidMovimiento);
-        return movimientosRepository.findByGuid(guidMovimiento).orElseThrow(
-                () -> new MovimientoNotFound(guidMovimiento)
-        );
+        return movimientosRepository.findByGuid(guidMovimiento)
+                .map(movimientosMapper::toMovimientoResponse)
+                .orElseThrow(() -> new MovimientoNotFound(guidMovimiento));
     }
 
     @Override
     @Cacheable(key = "#idCliente")
-    public Movimiento getByClienteGuid(String idCliente) {
-        log.info("Encontrando Movimientos por idCliente: {}", idCliente);
-        var cliente = clienteService.getById(idCliente);
-        if (cliente == null ) {
-            throw new ClienteNotFound(idCliente);
-        }
-        return movimientosRepository.findMovimientosByClienteGuid(idCliente)
-                .orElseThrow(() -> new ClienteHasNoMovements(idCliente));
+    public MovimientoResponse getByClienteGuid(String ClienteGuid) {
+        log.info("Encontrando Movimientos por idCliente: {}", ClienteGuid);
+        clienteService.getById(ClienteGuid);
+        return movimientosRepository.findMovimientosByClienteGuid(ClienteGuid)
+                .map(movimientosMapper::toMovimientoResponse)
+                .orElseThrow(() -> new ClienteHasNoMovements(ClienteGuid));
     }
 
     @Override
-    @CachePut(key = "#result.id")
-    public Movimiento save(MovimientoRequest movimientoRequest) {
+    @CachePut(key = "#result.guid")
+    public MovimientoResponse save(MovimientoRequest movimientoRequest) {
         log.info("Guardando Movimiento: {}", movimientoRequest);
-        var cliente = clienteService.getById(movimientoRequest.getIdCliente()).orElseThrow(() -> new ClienteNotFound(movimientoRequest.getIdCliente()));
-            // Se crea un nuevo movimiento
-            Movimiento movimiento = movimientoMapper.toMovimiento(movimientoRequest);
-            // Se guarda el movimiento
-            Movimiento savedMovimiento = movimientosRepository.save(movimiento);
-            // se setea cliente al movimiento
-            movimiento.setCliente(cliente);
-            // se guarda el movimiento
-           return movimientosRepository.save(movimiento);
+        clienteService.getById(movimientoRequest.getClienteGuid());
+        Movimiento movimiento = movimientosMapper.toMovimiento(movimientoRequest);
+        var savedMovimiento = movimientosRepository.save(movimiento);
+        return movimientosMapper.toMovimientoResponse(savedMovimiento);
     }
 }
