d12dc66c8cfd961ce1c8a0252ff56368f25a3db4
Álvaro Herero <alvaro.herrero11x@gmail.com>
2024-11-26 19:00:27 +0100

    Refactor Movimientos (New Movimiento Model)

--- Vives-Bank-Project/src/main/java/org/example/vivesbankproject/movimientos/dto/MovimientoResponse.java
+++ Vives-Bank-Project/src/main/java/org/example/vivesbankproject/movimientos/dto/MovimientoResponse.java
@@ -1,22 +1,4 @@
 package org.example.vivesbankproject.movimientos.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.example.vivesbankproject.cliente.dto.ClienteResponse;
-import org.example.vivesbankproject.movimientos.models.Transacciones;
-
-import java.util.List;
-
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class MovimientoResponse {
-    private String guid;
-    private String idUsuario;
-    private ClienteResponse clienteResponse;
-    private List<Transacciones> transacciones;
-    private Boolean isDeleted;
 }
---
+++ Vives-Bank-Project/src/main/java/org/example/vivesbankproject/movimientos/models/Movimiento.java
@@ -0,0 +1,54 @@
+package org.example.vivesbankproject.movimientos.models;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.example.vivesbankproject.cliente.dto.ClienteResponse;
+import org.example.vivesbankproject.utils.IdGenerator;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.TypeAlias;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document("movimientos")
+@TypeAlias("Movimiento")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Movimiento {
+    @Id
+    @Builder.Default
+    private ObjectId id = new ObjectId();
+
+    @Builder.Default
+    private String guid = IdGenerator.generarId();
+
+    private String clienteGuid;
+
+    private Domiciliacion domiciliacion;
+
+    private IngresoDeNomina ingresoDeNomina;
+
+    private PagoConTarjeta pagoConTarjeta;
+
+    private Transferencia transferencia;
+
+    @Builder.Default
+    private Boolean isDeleted = false;
+
+    @JsonProperty("id")
+    public String get_id() {
+        return id.toHexString();
+    }
+
+    @Builder.Default()
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+}
+
