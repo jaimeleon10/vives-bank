5a6179d7944272fceb01f46597a7b51d13881eb0
Álvaro Herero <alvaro.herrero11x@gmail.com>
2024-11-28 20:38:10 +0100

    new domiciliacion repository

---
+++ Vives-Bank-Project/src/main/java/org/example/vivesbankproject/movimientos/repositories/DomiciliacionRepository.java
@@ -0,0 +1,13 @@
+package org.example.vivesbankproject.movimientos.repositories;
+
+import org.bson.types.ObjectId;
+import org.example.vivesbankproject.movimientos.models.Domiciliacion;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface DomiciliacionRepository extends MongoRepository<Domiciliacion, ObjectId> {
+    Optional<Domiciliacion> findByGuid(String guid);
+}
