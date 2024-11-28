928647a0ccc94db411e3a1769faa2400eb43be1b
Álvaro Herero <alvaro.herrero11x@gmail.com>
2024-11-28 20:05:58 +0100

    Refactor models

--- Vives-Bank-Project/src/main/java/org/example/vivesbankproject/movimientos/models/Domiciliacion.java
+++ Vives-Bank-Project/src/main/java/org/example/vivesbankproject/movimientos/models/Domiciliacion.java
@@ -1,30 +1,58 @@
 package org.example.vivesbankproject.movimientos.models;
 
 import com.fasterxml.jackson.annotation.JsonTypeName;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.*;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
+import org.bson.types.ObjectId;
+import org.example.vivesbankproject.utils.generators.IdGenerator;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Document(collection = "domiciliaciones")
 public class Domiciliacion {
+    @Id
+    @Builder.Default
+    private ObjectId id = new ObjectId();
+
+    @Builder.Default
+    private String guid = IdGenerator.generarId();
+
     @Pattern(
             regexp = "^ES\\d{22}$",
             message = "El IBAN español debe comenzar con 'ES' seguido de 22 dígitos"
     )
-    @Size(
-            min = 24, max = 24,
-            message = "El IBAN español debe tener exactamente 24 caracteres"
-    )
-    private String iban_Origen;
+    private String ibanOrigen;
 
-    @Size(max = 100, message = "El nombre del acreedor no puede tener más de 100 caracteres")
+    @NotBlank
+    private String ibanDestino;
+
+    @Min(value = 1, message = "El importe no puede ser menor que 1")
+    @Max(value = 10000, message = "El importe no puede ser mayor que 1000000000000")
+    private BigDecimal cantidad;
+
+    @Size(max = 100, message = "El nombre del acreedor no puede superar los 100 caracteres")
     private String nombreAcreedor;
 
-    @Pattern(regexp = "^[A-Z0-9]{8}$", message = "El identificador del acreedor debe tener 8 caracteres alfanuméricos")
-    private String identificadorAcreedor;
+    @NotNull
+    private LocalDateTime fechaInicio;
 
+    @Enumerated(EnumType.STRING)
+    @Builder.Default
+    private Periodicidad periodicidad = Periodicidad.MENSUAL;
+
+    @Builder.Default
+    private Boolean activa = true;
+
+    @Builder.Default
+    private LocalDateTime ultimaEjecucion = LocalDateTime.now(); // Última vez que se realizó el cargo
 }
