e59354609446a2e2387764f1f3656aa835507f9c
Natalia González Álvarez <nagonal2004@gmail.com>
2024-12-03 11:15:30 +0100

    inicio de test de storageZip

---
+++ Vives-Bank-Project/src/test/java/org/example/vivesbankproject/storage/backupZip/controller/ZipStorageControllerTest.java
@@ -0,0 +1,34 @@
+package org.example.vivesbankproject.storage.backupZip.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ZipStorageControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void generateZip() {
+    }
+
+    @Test
+    void importFromZip() {
+    }
+
+    @Test
+    void serveFile() {
+    }
+
+    @Test
+    void deleteFile() {
+    }
+}
