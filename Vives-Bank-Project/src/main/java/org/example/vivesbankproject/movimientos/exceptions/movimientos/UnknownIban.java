602b3fd5f8365fce86999a5b1020fd69eb94633d
germanGFC <147338370+germangfc@users.noreply.github.com>
2024-12-07 12:40:22 +0100

    movimientos UnknownIban exception fix

--- Vives-Bank-Project/src/main/java/org/example/vivesbankproject/movimientos/exceptions/movimientos/UnknownIban.java
+++ Vives-Bank-Project/src/main/java/org/example/vivesbankproject/movimientos/exceptions/movimientos/UnknownIban.java
@@ -1,4 +1,12 @@
 package org.example.vivesbankproject.movimientos.exceptions.movimientos;
 
-public class UnknownIban {
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class UnknownIban extends MovimientosException{
+    public UnknownIban(String iban) {
+        super("El iban " + iban + " no es válido como Iban de origen");
+    }
 }
