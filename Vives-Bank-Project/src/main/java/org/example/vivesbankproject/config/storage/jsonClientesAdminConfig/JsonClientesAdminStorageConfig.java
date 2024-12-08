385703e2a77b06b9ee5131b2a0e54fa1eda1864c
Jaimelen10 <jleonmulero@gmail.com>
2024-12-08 17:14:34 +0100

    implementation fixes

--- Vives-Bank-Project/src/main/java/org/example/vivesbankproject/config/storage/jsonClientesAdminConfig/JsonClientesAdminStorageConfig.java
+++ Vives-Bank-Project/src/main/java/org/example/vivesbankproject/config/storage/jsonClientesAdminConfig/JsonClientesAdminStorageConfig.java
@@ -3,6 +3,7 @@ package org.example.vivesbankproject.config.storage.jsonClientesAdminConfig;
 import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.example.vivesbankproject.rest.storage.jsonClientes.services.JsonClientesStorageService;
+import org.example.vivesbankproject.rest.storage.jsonClientesAdmin.services.JsonClientesAdminFileSystemStorage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
