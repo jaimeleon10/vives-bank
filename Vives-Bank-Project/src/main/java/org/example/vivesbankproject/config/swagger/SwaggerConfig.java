bca9689b695fd70816a22a063a2f8d87f97ae78e
Jaimelen10 <jleonmulero@gmail.com>
2024-12-09 14:24:45 +0100

    fixed swagger config

--- Vives-Bank-Project/src/main/java/org/example/vivesbankproject/config/swagger/SwaggerConfig.java
+++ Vives-Bank-Project/src/main/java/org/example/vivesbankproject/config/swagger/SwaggerConfig.java
@@ -36,20 +36,45 @@ class SwaggerConfig {
                                 .description("API de un proyecto de un banco de 2º DAW. 2024")
                                 .contact(
                                         new Contact()
-                                                .name("José Luis González Sánchez")
-                                                .url("https://joseluisgs.dev")
+                                                .name("Jaime León Mulero")
+                                                .url("https://github.com/jaimeleon10")
+                                )
+                                .contact(
+                                        new Contact()
+                                                .name("Natalia González Álvarez")
+                                                .url("https://github.com/ngalvez0910")
+                                )
+                                .contact(
+                                        new Contact()
+                                                .name("Alba García Orduña")
+                                                .url("https://github.com/Alba448")
+                                )
+                                .contact(
+                                        new Contact()
+                                                .name("Álvaro Herrero")
+                                                .url("https://github.com/alvarito304")
+                                )
+                                .contact(
+                                        new Contact()
+                                                .name("Germán Fernández Carrecedo")
+                                                .url("https://github.com/germangfc")
+                                )
+                                .contact(
+                                        new Contact()
+                                                .name("Mario de Domingo Álvarez")
+                                                .url("https://github.com/wolverine307mda")
                                 )
 
                 )
                 .externalDocs(
                         new ExternalDocumentation()
                                 .description("Documentación del Proyecto")
-                                .url("https://github.com/joseluisgs/DesarrolloWebEntornosServidor-02-2023-2024")
+                                .url("https://github.com/jaimeleon10/vives-bank/Documentacion")
                 )
                 .externalDocs(
                         new ExternalDocumentation()
                                 .description("GitHub del Proyecto")
-                                .url("https://github.com/joseluisgs/DesarrolloWebEntornosServidor-02-Proyecto-2023-2024")
+                                .url("https://github.com/jaimeleon10/vives-bank")
                 )
                 // Añadimos la seguridad JWT
                 .addSecurityItem(new SecurityRequirement().
