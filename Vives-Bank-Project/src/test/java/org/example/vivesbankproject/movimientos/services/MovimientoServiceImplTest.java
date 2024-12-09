a1b573ae355bb75dc9b3ed1f42ef19423e20f755
Álvaro Herero <alvaro.herrero11x@gmail.com>
2024-12-09 23:41:07 +0100

    Test

--- Vives-Bank-Project/src/test/java/org/example/vivesbankproject/movimientos/services/MovimientoServiceImplTest.java
+++ Vives-Bank-Project/src/test/java/org/example/vivesbankproject/movimientos/services/MovimientoServiceImplTest.java
@@ -1,20 +1,39 @@
 package org.example.vivesbankproject.movimientos.services;
 
 import org.bson.types.ObjectId;
+import org.example.vivesbankproject.rest.cliente.dto.ClienteResponse;
 import org.example.vivesbankproject.rest.cliente.exceptions.ClienteNotFoundByUser;
+import org.example.vivesbankproject.rest.cliente.models.Cliente;
 import org.example.vivesbankproject.rest.cliente.service.ClienteService;
+import org.example.vivesbankproject.rest.cuenta.dto.cuenta.CuentaRequestUpdate;
+import org.example.vivesbankproject.rest.cuenta.dto.cuenta.CuentaResponse;
+import org.example.vivesbankproject.rest.cuenta.exceptions.cuenta.CuentaNotFound;
+import org.example.vivesbankproject.rest.cuenta.mappers.CuentaMapper;
+import org.example.vivesbankproject.rest.cuenta.models.Cuenta;
 import org.example.vivesbankproject.rest.cuenta.services.CuentaService;
 import org.example.vivesbankproject.rest.movimientos.dto.MovimientoRequest;
 import org.example.vivesbankproject.rest.movimientos.dto.MovimientoResponse;
+import org.example.vivesbankproject.rest.movimientos.exceptions.domiciliacion.DuplicatedDomiciliacionException;
 import org.example.vivesbankproject.rest.movimientos.exceptions.movimientos.ClienteHasNoMovements;
 import org.example.vivesbankproject.rest.movimientos.exceptions.movimientos.MovimientoNotFound;
+import org.example.vivesbankproject.rest.movimientos.exceptions.movimientos.NegativeAmount;
+import org.example.vivesbankproject.rest.movimientos.exceptions.movimientos.UnknownIban;
 import org.example.vivesbankproject.rest.movimientos.mappers.MovimientoMapper;
+import org.example.vivesbankproject.rest.movimientos.models.Domiciliacion;
+import org.example.vivesbankproject.rest.movimientos.models.IngresoDeNomina;
 import org.example.vivesbankproject.rest.movimientos.models.Movimiento;
+import org.example.vivesbankproject.rest.movimientos.models.Periodicidad;
 import org.example.vivesbankproject.rest.movimientos.repositories.DomiciliacionRepository;
 import org.example.vivesbankproject.rest.movimientos.repositories.MovimientosRepository;
 import org.example.vivesbankproject.rest.movimientos.services.MovimientosServiceImpl;
 import org.example.vivesbankproject.rest.tarjeta.service.TarjetaService;
 import org.example.vivesbankproject.config.websockets.WebSocketConfig;
+import org.example.vivesbankproject.rest.users.dto.UserResponse;
+import org.example.vivesbankproject.rest.users.models.User;
+import org.example.vivesbankproject.rest.users.services.UserService;
+import org.example.vivesbankproject.websocket.notifications.dto.DomiciliacionResponse;
+import org.example.vivesbankproject.websocket.notifications.mappers.NotificationMapper;
+import org.example.vivesbankproject.websocket.notifications.models.Notification;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -26,6 +45,9 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -52,19 +74,49 @@ class MovimientoServiceImplTest {
     private CuentaService cuentaService;
 
     @Mock
+    private CuentaMapper cuentaMapper;
+
+    @Mock
+    private NotificationMapper notificationMapper;
+
+    @Mock
     private MovimientoMapper movimientosMapper;
 
     @Mock
     private WebSocketConfig webSocketConfig;
+
+    @Mock
+    private UserService userService;
 
     @InjectMocks
     private MovimientosServiceImpl movimientosService;
 
     private Movimiento movimiento;
     private MovimientoResponse movimientoResponse;
+    private Domiciliacion domiciliacion;
+    CuentaResponse cuenta = new CuentaResponse();
+    ClienteResponse clienteResponse = new ClienteResponse();
+    UserResponse userResponse = new UserResponse();
 
     @BeforeEach
     void setUp() {
+
+        userResponse.setUsername("username123");
+
+        clienteResponse.setUserId("user123");
+        clienteResponse.setGuid("client123");
+
+        cuenta.setIban("ES60123412347246753334");
+        cuenta.setSaldo("100.00");
+        cuenta.setClienteId("client123");
+
+        domiciliacion = new Domiciliacion();
+        domiciliacion.setActiva(true);
+        domiciliacion.setUltimaEjecucion(LocalDateTime.now().minusDays(2));
+        domiciliacion.setPeriodicidad(Periodicidad.DIARIA);
+        domiciliacion.setIbanOrigen("ES60123412347246753334");
+        domiciliacion.setCantidad(new BigDecimal("100.00"));
+
         movimiento = Movimiento.builder()
                 .clienteGuid("client123")
                 .build();
@@ -199,5 +251,235 @@ class MovimientoServiceImplTest {
 
         assertThrows(ClienteNotFoundByUser.class,
                 () -> movimientosService.save(request));
+    }
+
+    @Test
+    void testSaveDomiciliacion_Success() {
+        // Arrange
+        User user = User.builder()
+                .guid(userResponse.getGuid())
+                .username(userResponse.getUsername())
+                .password(userResponse.getPassword())
+                .roles(userResponse.getRoles())
+                .build();
+
+        DomiciliacionResponse domiciliacionResponse = new DomiciliacionResponse(
+                domiciliacion.getGuid(),
+                domiciliacion.getIbanOrigen(),
+                domiciliacion.getIbanDestino(),
+                domiciliacion.getCantidad(),
+                domiciliacion.getNombreAcreedor(),
+                domiciliacion.getFechaInicio().toString(),
+                domiciliacion.getPeriodicidad().toString(),
+                domiciliacion.getActiva(),
+                domiciliacion.getUltimaEjecucion().toString()
+        );
+
+
+        when(clienteService.getUserAuthenticatedByGuid(userResponse.getGuid())).thenReturn(clienteResponse);
+        when(cuentaService.getByIban(domiciliacion.getIbanOrigen())).thenReturn(cuenta);
+        when(domiciliacionRepository.findByClienteGuid(anyString())).thenReturn(Optional.empty());
+        when(domiciliacionRepository.save(any(Domiciliacion.class))).thenReturn(domiciliacion);
+        when(notificationMapper.toDomiciliacionDto(domiciliacion)).thenReturn(domiciliacionResponse);
+        when(clienteService.getById(anyString())).thenReturn(clienteResponse);
+        when(userService.getById(anyString())).thenReturn(userResponse); // Add this line to mock the userService call
+
+        // Act
+        Domiciliacion result = movimientosService.saveDomiciliacion(user, domiciliacion);
+
+        // Assert
+        assertAll(
+                () -> assertNotNull(result),
+                () -> assertEquals(domiciliacionResponse.guid(), result.getGuid())
+        );
+
+        verify(domiciliacionRepository).save(domiciliacion);
+    }
+
+    @Test
+    void testSaveDomiciliacion_ClienteNotFound() {
+        // Arrange
+        User user = User.builder()
+                .guid(userResponse.getGuid())
+                .username(userResponse.getUsername())
+                .password(userResponse.getPassword())
+                .roles(userResponse.getRoles())
+                .build();
+
+        when(clienteService.getUserAuthenticatedByGuid(userResponse.getGuid())).thenReturn(null);
+
+        // Act
+        var result = assertThrows(ClienteNotFoundByUser.class, () -> movimientosService.saveDomiciliacion(user, domiciliacion));
+
+        // Assert
+        assertEquals(ClienteNotFoundByUser.class, result.getClass());
+
+        verify(domiciliacionRepository, times(0)).save(domiciliacion);
+    }
+
+    @Test
+    void testSaveDomiciliacion_CuentaNotFound() {
+        // Arrange
+        User user = User.builder()
+                .guid(userResponse.getGuid())
+                .username(userResponse.getUsername())
+                .password(userResponse.getPassword())
+                .roles(userResponse.getRoles())
+                .build();
+
+        when(clienteService.getUserAuthenticatedByGuid(userResponse.getGuid())).thenReturn(clienteResponse);
+        when(cuentaService.getByIban(domiciliacion.getIbanOrigen())).thenReturn(null);
+
+        // Act
+        var result = assertThrows(CuentaNotFound.class, () -> movimientosService.saveDomiciliacion(user, domiciliacion));
+        // Assert
+        assertEquals(CuentaNotFound.class, result.getClass());
+
+        verify(domiciliacionRepository, times(0)).save(domiciliacion);
+    }
+
+    @Test
+    void testSaveDomiciliacion_IbanOrigenDiferenteQuienHaceLaPeticion() {
+        // Arrange
+        User user = User.builder()
+                .guid(userResponse.getGuid())
+                .username(userResponse.getUsername())
+                .password(userResponse.getPassword())
+                .roles(userResponse.getRoles())
+                .build();
+
+        clienteResponse.setGuid("different-user-guid");
+        when(clienteService.getUserAuthenticatedByGuid(userResponse.getGuid())).thenReturn(clienteResponse);
+        when(cuentaService.getByIban(domiciliacion.getIbanOrigen())).thenReturn(cuenta);
+
+        // Act
+        var result = assertThrows(UnknownIban.class, () -> movimientosService.saveDomiciliacion(user, domiciliacion));
+        // Assert
+        assertEquals(UnknownIban.class, result.getClass());
+
+        verify(domiciliacionRepository, times(0)).save(domiciliacion);
+    }
+
+    @Test
+    void testSaveDomiciliacion_DuplicatedDomiciliacion() {
+        // Arrange
+        User user = User.builder()
+                .guid(userResponse.getGuid())
+                .username(userResponse.getUsername())
+                .password(userResponse.getPassword())
+                .roles(userResponse.getRoles())
+                .build();
+
+        // Ensure domiciliacion has a non-null ibanDestino
+        domiciliacion.setIbanDestino("ES60123412347246753335");
+
+        when(clienteService.getUserAuthenticatedByGuid(userResponse.getGuid())).thenReturn(clienteResponse);
+        when(cuentaService.getByIban(domiciliacion.getIbanOrigen())).thenReturn(cuenta);
+        when(domiciliacionRepository.findByClienteGuid(anyString())).thenReturn(Optional.ofNullable(domiciliacion));
+
+        // Act
+        var result = assertThrows(DuplicatedDomiciliacionException.class, () -> movimientosService.saveDomiciliacion(user, domiciliacion));
+        // Assert
+        assertEquals(DuplicatedDomiciliacionException.class, result.getClass());
+
+        verify(domiciliacionRepository, times(0)).save(domiciliacion);
+    }
+
+    @Test
+    void testSaveDomiciliacion_NegativeAmount() {
+        // Arrange
+        User user = User.builder()
+                .guid(userResponse.getGuid())
+                .username(userResponse.getUsername())
+                .password(userResponse.getPassword())
+                .roles(userResponse.getRoles())
+                .build();
+
+        // Ensure domiciliacion has a non-null ibanDestino
+        domiciliacion.setIbanDestino("ES60123412347246753335");
+        domiciliacion.setCantidad(BigDecimal.valueOf(-250.75)); // Negative amount
+
+        when(clienteService.getUserAuthenticatedByGuid(userResponse.getGuid())).thenReturn(clienteResponse);
+        when(cuentaService.getByIban(domiciliacion.getIbanOrigen())).thenReturn(cuenta);
+        when(domiciliacionRepository.findByClienteGuid(anyString())).thenReturn(Optional.empty());
+
+        // Act
+        var result = assertThrows(NegativeAmount.class, () -> movimientosService.saveDomiciliacion(user, domiciliacion));
+        // Assert
+        assertEquals(NegativeAmount.class, result.getClass());
+
+        verify(domiciliacionRepository, times(0)).save(domiciliacion);
+    }
+
+
+    @Test
+    void testSaveIngresoDeNomina_Success() {
+        // Arrange
+        User user = User.builder()
+                .guid("user-guid")
+                .build();
+
+        IngresoDeNomina ingresoDeNomina = new IngresoDeNomina();
+        ingresoDeNomina.setIban_Destino("ES60123412347246753334");
+        ingresoDeNomina.setIban_Origen("ES60123412347246753335");
+        ingresoDeNomina.setCifEmpresa("B12345678");
+        ingresoDeNomina.setCantidad(100.00);
+
+
+        Movimiento movimiento = Movimiento.builder()
+                .clienteGuid("client-guid")
+                .ingresoDeNomina(ingresoDeNomina)
+                .build();
+
+        MovimientoResponse expectedResponse = MovimientoResponse.builder()
+                .clienteGuid("client-guid")
+                .ingresoDeNomina(ingresoDeNomina)
+                .build();
+
+        when(clienteService.getUserAuthenticatedByGuid(user.getGuid())).thenReturn(clienteResponse);
+        when(cuentaService.getByIban(ingresoDeNomina.getIban_Destino())).thenReturn(cuenta);
+        when(movimientosRepository.save(any(Movimiento.class))).thenReturn(movimiento);
+        when(movimientosMapper.toMovimientoResponse(movimiento)).thenReturn(expectedResponse);
+        when(cuentaMapper.toCuentaRequestUpdate(any())).thenReturn(new CuentaRequestUpdate());
+        when(cuentaService.getByIban(domiciliacion.getIbanOrigen())).thenReturn(cuenta);
+        when(clienteService.getById(anyString())).thenReturn(clienteResponse);
+        when(userService.getById(anyString())).thenReturn(userResponse);
+
+        // Act
+        MovimientoResponse actualResponse = movimientosService.saveIngresoDeNomina(user, ingresoDeNomina);
+
+        // Assert
+        assertAll(
+                () -> assertEquals(expectedResponse.getClienteGuid(), actualResponse.getClienteGuid()),
+                () -> assertEquals(expectedResponse.getIngresoDeNomina(), actualResponse.getIngresoDeNomina()),
+                () -> assertNotEquals(null, actualResponse.getIngresoDeNomina())
+        );
+
+        verify(movimientosRepository, times(1)).save(any(Movimiento.class));
+    }
+
+    @Test
+    void testSaveIngresoDeNomina_ClienteNotFound() {
+        // Arrange
+        User user = User.builder()
+                .guid("user-guid")
+                .build();
+
+        IngresoDeNomina ingresoDeNomina = new IngresoDeNomina();
+        ingresoDeNomina.setIban_Destino("ES60123412347246753334");
+        ingresoDeNomina.setIban_Origen("ES60123412347246753335");
+        ingresoDeNomina.setCifEmpresa("B12345678");
+        ingresoDeNomina.setCantidad(100.00);
+
+
+        when(clienteService.getUserAuthenticatedByGuid(user.getGuid())).thenReturn(null);
+
+        // Act
+        var result = assertThrows(ClienteNotFoundByUser.class, () -> movimientosService.saveIngresoDeNomina(user, ingresoDeNomina));
+
+        // Assert
+        assertEquals(ClienteNotFoundByUser.class, result.getClass());
+
+        verify(movimientosRepository, times(0)).save(any(Movimiento.class));
     }
 }
